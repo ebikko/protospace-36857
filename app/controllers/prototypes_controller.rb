b585ed52fafe96f33915e9ac1b8687a5236e9d48
ebikko <c27a258ee9ye9@gmail.com>
2021-10-26 16:56:05 +0900

    first commit 2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update, :index]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action :index
+      redirect_to root_path
     end
   end
 end
